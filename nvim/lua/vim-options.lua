d30758b7576fbedc7b9b761d657a03219258b989
Mingyu Jung <gnaroshi@Mingyus-MacBook-Pro.local>
2024-04-04 23:09:08 +0900

    [UPDATE][config] iterm2, nvim, skhd, thefuck, yabai setting backups

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,19 @@
+-- vim setups
+vim.cmd("set ruler")
+vim.cmd("set showcmd")
+vim.cmd("set wildmenu")
+
+vim.cmd("set nu")
+vim.cmd("set relativenumber")
+
+vim.cmd("set expandtab")
+vim.cmd("set smarttab")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+
+vim.cmd("set history=500")
+vim.g.mapleader = " "
